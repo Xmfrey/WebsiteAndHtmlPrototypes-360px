10a7e6ecafb0174fa8dbd8907b3678e3f5e50f6a
Xmfrey <ser071095@yandex.ru>
2024-01-30 14:51:08 +0300

    data updated

--- app/js/app2.js
+++ app/js/app2.js
@@ -3,14 +3,22 @@ const tabsСontent = document.querySelectorAll(".tab__content");
 
 tabs.forEach((tab, index) => {
   tab.addEventListener("click", () => {
-    tabs.forEach((tab) => {
-      tab.classList.remove("active");
-    });
-    tabsСontent.forEach((content) => {
-      content.classList.remove("active");
-    });
-
-    tab.classList.add("active");
-    tabsСontent[index].classList.add("active");
+    tabAction(tab, index);
+  });
+  tab.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      tabAction(tab, index);
+    }
   });
 });
+
+function tabAction(tab, index) {
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+  tabsСontent.forEach((content) => {
+    content.classList.remove("active");
+  });
+  tab.classList.add("active");
+  tabsСontent[index].classList.add("active");
+}
